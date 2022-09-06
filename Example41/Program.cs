b68ae28ea9161fada0c9f7536021745613d098dc
Irek <general157@mail.ru>
2022-09-06 21:54:38 +0300

    add example 41

---
+++ Example41/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+int GetPositiveCount()
+{
+    int[] numbers = Console.ReadLine().Split(", ").Select(x => Convert.ToInt32(x)).ToArray();
+    int count = 0;
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        if (numbers[i] > 0) count++;
+    }
+    return count;
+}
+
+try
+{
+    Console.Write("Введите числа через ', ': \t");
+    Console.WriteLine($"Количество положительных чисел:\t{GetPositiveCount()}");
+}
+catch
+{
+    Console.WriteLine("Неверный ввод!");
+}
+
